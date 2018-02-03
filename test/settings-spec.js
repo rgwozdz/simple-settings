45c993ed7577ddac3abe64a1bab5c2ffb485dd6a
Rich Gwozdz <rgwozdz@spatialdev.com>
2018-02-03 14:55:55 -0800

    Add test spec.

---
+++ test/settings-spec.js
@@ -0,0 +1,31 @@
+"use strict";
+const chai = require("chai");
+const expect = chai.expect;
+
+describe('Settings suite', function () {
+
+  describe('Instantiate Setting class', function(){
+
+    it("Setting should instantiate without error", function () {
+
+    });
+
+  });
+
+  describe('Setting "get" method', function(){
+
+    it("Should return top-level primitives", function () {
+
+    });
+
+  });
+
+
+  describe('toJS() method', function(){
+
+    it("Plain old object should get returned", function () {
+
+    });
+
+  });
+});
